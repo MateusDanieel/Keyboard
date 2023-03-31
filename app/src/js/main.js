e26316228ac121608ac4bc979f97daf929e92777
Mateus Daniel <mateus.silva@tritone.com.br>
2023-03-31 16:08:26 -0300

    fix: fix bug print screen key

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -1,7 +1,8 @@
 (function() {
     'use strict';
     
-    var keyboardKeys = document.querySelectorAll('.sec-main__keyboard__row__key');
+    const keyboardKeys = document.querySelectorAll('.sec-main__keyboard__row__key');
+    const keyboardFunctKeys = [44, 112, 114, 116, 117, 118, 121, 122, 123];
     
     keyboardKeys.forEach((keyboardKey) => {
         
@@ -9,10 +10,36 @@
             keyboardKey.setAttribute('style', 'font-size: 10px !important;');
         }
 
-        document.addEventListener('keypress', function(e) {
-            console.log(e.keyCode)
+        document.addEventListener('keydown', function(e) {
+            let code = e.which || e.keyCode;
 
+            keyboardFunctKeys.forEach((f) => {
+                if (code == f) {
+                    e.preventDefault();
+                }
+            })
+            
             if (keyboardKey.innerText == e.key || keyboardKey.dataset.keycode == e.keyCode) {
+
+                if (keyboardKey.classList.contains('checked')) {
+
+                    keyboardKey.classList.remove('checked');
+
+                } else {
+
+                    keyboardKey.classList.add('checked');
+
+                }
+
+            }
+
+        });
+
+        // FIX - tecla 'print screen' por alguma razão não funciona com 'keydown'
+
+        document.addEventListener('keyup', function(e) {
+           
+            if (keyboardKey.dataset.keycode == '44' && keyboardKey.dataset.keycode == e.keyCode) {
 
                 if (keyboardKey.classList.contains('checked')) {
                     keyboardKey.classList.remove('checked');
@@ -23,6 +50,8 @@
             }
 
         });
+
+
 
     });
 })();
